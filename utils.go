569f6f9f8ae36b94547e956e7f46da18a30d8497
Evgeniy Vasilev <aquilax@gmail.com>
2014-11-01 06:55:08 +0100

    Revert "Revert "Addressed linter errors""

--- utils.go
+++ utils.go
@@ -8,30 +8,30 @@ import (
 )
 
 const (
-	SEPARATOR_PATH = "/"
+	separatorPath = "/"
 )
 
 type RequestParams map[string]string
 
 func getPath(segments []string) string {
-	segments = append([]string{API_SEGMENT, API_VERSION}, segments...)
-	return strings.Join(segments, SEPARATOR_PATH)
+	segments = append([]string{apiSegment, apiVersion}, segments...)
+	return strings.Join(segments, separatorPath)
 }
 
-func (fapi *FyndiqApi) getUrl(path string, params RequestParams) string {
-	parsedUrl, _ := url.Parse(URL)
+func (fapi *FyndiqAPI) getURL(path string, params RequestParams) string {
+	parsedURL, _ := url.Parse(apiURL)
 	values := url.Values{}
 	values.Set("user", fapi.user)
 	values.Set("token", fapi.token)
 	for name, value := range params {
 		values.Set(name, value)
 	}
-	parsedUrl.Path = path
-	parsedUrl.RawQuery = values.Encode()
-	return parsedUrl.String()
+	parsedURL.Path = path
+	parsedURL.RawQuery = values.Encode()
+	return parsedURL.String()
 }
 
-func getJsonBlob(url string) ([]byte, error) {
+func getJSONBlob(url string) ([]byte, error) {
 	response, err := http.Get(url)
 	if err != nil {
 		return nil, err
